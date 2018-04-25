c773b44eb89d147f384ae8a2c005a87ef67d1c21
Ross Khapilov <rossxpanteley@gmail.com>
2018-04-25 14:01:17 +0300

    Решил простую задачку

---
+++ UniqueNumber.java
@@ -0,0 +1,29 @@
+package exercises.codewars;
+
+import java.util.Arrays;
+
+/**
+ * There is an array with some numbers. All numbers are equal except for one. Try to find it!
+ * <p>
+ * Kata.findUniq(new double[]{ 1, 1, 1, 2, 1, 1 }); // => 2
+ * Kata.findUniq(new double[]{ 0, 0, 0.55, 0, 0 }); // => 0.55
+ * It’s guaranteed that array contains more than 3 numbers.
+ * <p>
+ * The tests contain some very huge arrays, so think about performance.
+ *
+ * @author Ross Khapilov
+ * @version 1.0 created on 25.04.2018
+ */
+public class UniqueNumber {
+    public static double findUniq(double arr[]) {
+        Arrays.sort(arr);
+        return arr[0] != arr[1] ? arr[0] : arr[arr.length - 1];
+    }
+
+    public static void main(String[] args) {
+        double[] doubles = {1, 1, 1, 4};
+        double a = findUniq(doubles);
+        System.out.println(a);
+        System.out.println(Arrays.toString(doubles));
+    }
+}
