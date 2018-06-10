2ee45b70ce39bbed540cd4525df537624de6dcab
Ross Khapilov <rossxpanteley@gmail.com>
2018-06-10 20:17:26 +0300

    easy kyu7

---
+++ kyu7/DeltaBits.java
@@ -0,0 +1,27 @@
+package exercises.codewars.kyu7;
+
+/**
+ * Complete the function to determine the number of bits required to convert integer A to integer B (where A and B >= 0)
+ * <p>
+ * The upper limit for A and B is 216, int.MaxValue or similar.
+ * <p>
+ * For example, you can change 31 to 14 by flipping the 4th and 0th bit:
+ * <p>
+ * 31  0 0 0 1 1 1 1 1
+ * 14  0 0 0 0 1 1 1 0
+ * ---  ---------------
+ * bit  7 6 5 4 3 2 1 0
+ * Thus 31 and 14 should return 2.
+ *
+ * @author Ross Khapilov
+ * @version 1.0 10.06.2018
+ */
+public class DeltaBits {
+    public static int convertBits(int a, int b) {
+        return Integer.bitCount(a ^ b);
+    }
+
+    public static void main(String[] args) {
+        convertBits(31, 14);
+    }
+}
