3b3319cc1fc9d03e62a002306a028cfdadb03ee4
Ross Khapilov <rossxpanteley@gmail.com>
2018-07-02 23:46:24 +0300

    easy

---
+++ kyu8/YesOrNo.java
@@ -0,0 +1,13 @@
+package exercises.codewars.kyu8;
+
+/**
+ * Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+ *
+ * @author Ross Khapilov
+ * @version 1.0 02.07.2018
+ */
+public class YesOrNo {
+    public static String boolToWord(boolean b) {
+        return b ? "Yes" : "No";
+    }
+}
