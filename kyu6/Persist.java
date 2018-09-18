9d30581a2dad023d68b9ecf733b05d8f16ef538b
Ross Khapilov <rossxpanteley@gmail.com>
2018-09-18 23:35:05 +0300

    easy

---
+++ kyu6/Persist.java
@@ -0,0 +1,30 @@
+package exercises.codewars.kyu6;
+
+/**
+ * Write a function, persistence, that takes in a positive parameter num and returns its multiplicative
+ * persistence, which is the number of times you must multiply the digits in num until you reach
+ * a single digit.
+ * <p>
+ * For example:
+ * <p>
+ * persistence(39) == 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+ * // and 4 has only one digit
+ * <p>
+ * persistence(999) == 4 // because 9*9*9 = 729, 7*2*9 = 126,
+ * // 1*2*6 = 12, and finally 1*2 = 2
+ * <p>
+ * persistence(4) == 0 // because 4 is already a one-digit number
+ *
+ * @author Ross Khapilov
+ * @version 1.0 18.09.2018
+ */
+public class Persist {
+    public static int persistence(long n) {
+        long m = 1, r = n;
+        if (r / 10 == 0)
+            return 0;
+        for (r = n; r != 0; r /= 10)
+            m *= r % 10;
+        return persistence(m) + 1;
+    }
+}
