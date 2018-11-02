4e02cff8e0f754de25c998d30d708d03e662a5fe
Ross Khapilov <rossxpanteley@gmail.com>
2018-11-02 23:35:41 +0300

    easy

---
+++ kyu7/ThinkingAndTesting.java
@@ -0,0 +1,12 @@
+package exercises.codewars.kyu7;
+
+/**
+ * @author Ross Khapilov
+ * @version 1.0 02.11.2018
+ */
+public class ThinkingAndTesting {
+    public static int testAB(int a, int b) {
+        // Good luck!
+        return a | b; // hmm ...is that right?
+    }
+}
